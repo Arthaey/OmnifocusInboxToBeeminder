b89d00badfe7140959109153f798cdb56fc962c1
Arthaey <arthaey@gmail.com>
2014-03-14 05:14:06 -0700

    Supports counting inbox, due_soon, or overdue tasks. Fairly large code changes, including creating a new helper class.

--- omnifocustobeeminder.rb
+++ omnifocustobeeminder.rb
@@ -2,11 +2,17 @@ require "optparse"
 require "beeminder"
 require "sqlite3"
 require "etc"
+require_relative "omnifocus"
 
 # Get command line options and parse them
+COUNT_OPTIONS = %w[inbox due_soon overdue]
 cmd_line_options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: omnifocustobeeminder.rb [options]"
+
+  opts.on("-c", "--count TASKS", COUNT_OPTIONS, "#Which type of tasks to count", " (#{COUNT_OPTIONS})") do |which|
+    cmd_line_options[:which_tasks] = which
+  end
 
   opts.on("-k key", "--apikey KEY", "#Your Beeminder API key") do |key|
     cmd_line_options[:key] = key
@@ -20,12 +26,22 @@ OptionParser.new do |opts|
     cmd_line_options[:path] = path
   end
 
+  opts.on("-v", "--verbose", "#Output verbose messages") do
+    cmd_line_options[:verbose] = true
+  end
+
   opts.on_tail("-h", "--help", "#Show this message") do
     puts opts
     exit
   end
 
 end.parse!
+
+# Terminate the script if which type of tasks to count was not provided by the user
+if cmd_line_options[:which_tasks].nil?
+  puts "ERROR: You must choose to --count one of #{COUNT_OPTIONS}."
+  exit
+end
 
 # Terminate the script if the key was not provided by the user
 if cmd_line_options[:key].nil?
@@ -50,33 +66,28 @@ else
 end
 
 # Open the OmniFocus database
+count = nil
 begin
   db_connection = SQLite3::Database.new database_path
+  omnifocus = Omnifocus.new db_connection, cmd_line_options[:verbose]
+  # Call inbox_count, due_soon_count, or overdue_count as appropriate
+  count = omnifocus.send "#{cmd_line_options[:which_tasks]}_count"
 rescue
   puts "ERROR: Your Omnifocus database could not be opened."
   puts "DATABASE: #{database_path}"
   exit
 end
 
-# Query the OmniFocus database for inbox tasks
-begin
-  # Get all inbox actions in the inbox that are not action groups
-  inbox_actions = db_connection.execute "select count(*) from task where (inInbox=1) and (dateCompleted is null);"
-  # Get all inbox action groups and count them in too
-  inbox_action_groups = db_connection.execute "select count(*) from task where (inInbox=1) and (childrenCount>0);"
-rescue
-  puts "ERROR: Your Omnifocus database could not be queried."
+if count.nil?
+  puts "ERROR: Count of #{cmd_line_options[:which_tasks]} tasks could not be determined."
   exit
 end
-
-# Add up the inbox actions and the inbox action groups
-all_actions = inbox_actions.first.first.to_i + inbox_action_groups.first.first.to_i
 
 # Connect to Beeminder and post the datapoint
 beeminder_connection = Beeminder::User.new cmd_line_options[:key]
 omnifocus_inbox_tracking_goal = beeminder_connection.goal cmd_line_options[:goal]
 new_omnifocus_inbox_count_data_point = Beeminder::Datapoint.new(
-  :value => all_actions,
-  :comment => "Automated Omnifocus count as of #{Time.now.strftime("%m/%d/%Y at %I:%M %p")}"
+  :value => count,
+  :comment => "Automated Omnifocus count of #{cmd_line_options[:which_tasks]} as of #{Time.now.strftime("%m/%d/%Y at %I:%M %p")}"
 )
 omnifocus_inbox_tracking_goal.add new_omnifocus_inbox_count_data_point
